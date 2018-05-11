154d0d707d2622e27147a1dc1534cb08d5756eb6
usopp <usopp9@naver.com>
2018-05-11 14:33:25 +0900

    coffee_setting완료

---
+++ src/kr/or/dgit/erp_setting/TestMain.java
@@ -0,0 +1,34 @@
+package kr.or.dgit.erp_setting;
+
+import java.sql.SQLException;
+import java.util.Map.Entry;
+import java.util.Properties;
+
+import kr.or.dgit.erp_application.jdbc.DBCon;
+import kr.or.dgit.erp_application.jdbc.LoadProperties;
+
+public class TestMain {
+
+	public static void main(String[] args) throws SQLException, InterruptedException {
+		testDBConnection();
+	}
+	
+	private static void testDBConnection() {
+		DBCon dbCon = DBCon.getInstance();
+		System.out.println(dbCon);
+
+		LoadProperties lp = new LoadProperties();
+		Properties pro = lp.getProperties();
+		
+		for(Entry<Object,Object> e: pro.entrySet()) {
+			System.out.printf("%s : %s%n", e.getKey(), e.getValue());
+		}
+		
+		
+		String[] values = pro.get("tables").toString().split(",");
+		for(String v : values) {
+			System.out.println(v);
+		}
+	}
+
+}
---
+++ src/kr/or/dgit/erp_setting/service/DaoService.java
@@ -0,0 +1,5 @@
+package kr.or.dgit.erp_setting.service;
+
+public interface DaoService {
+	void service();
+}
